50a5d553bd234ae46aaf0a840c50816521d2e96e
tracer <tracer@192-168-1-8.tpgi.com.au>
2016-09-24 12:14:12 +1000

    sync with OSX notification

---
+++ bin/sync_with_notification.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+require_relative '../src/board'
+require_relative '../src/downloader'
+require_relative '../src/pin'
+
+def board_id
+  ARGV[0]
+end
+
+def pinterest_api_token
+  ARGV[1]
+end
+
+def download_folder
+  ARGV[2]
+end
+
+def board_title
+  ARGV[3]
+end
+
+def board
+  Board.new(id: board_id, api_token: pinterest_api_token)
+end
+
+def send_notification
+  `osascript -e 'display notification "Synced #{board_title} board" with title "Pinterest Sync"'`
+end
+
+begin
+  downloader = Downloader.new(board: board, download_path: download_folder)
+  downloader.run
+  send_notification
+end
